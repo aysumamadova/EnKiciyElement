77499f40789b9093d35223eb08c55aa9a57e5caf
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-05 11:46:13 +0400

    first commit

---
+++ task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -6, 0, 7, 9 };
+            int small = 0;
+            small = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (small > arr[i])
+                {
+                    small = arr[i];
+                }
+            }
+            Console.WriteLine("Ən kiçik element: " + small);
+
+
+
+
+
+
+        }
+    }
+}
